60450c9eb530e5086f592215dc6a6981e4434727
Jayamal <jayamaljayamaha2@gmail.com>
2023-05-22 21:56:39 +0530

    fix: incorporate feedback comments
    1) add env file to get config values

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,12 @@ import {RequestDto} from "./request.dto";
 import minimist from "minimist"
 import {getImage, mergeImage, saveImage} from "./image.service"
 import {validateNumber, validateString} from "./validation.service";
+import dotenv from 'dotenv';
+dotenv.config();
 
 const argv = minimist(process.argv.slice(2))
+const baseUrl = process.env.CAT_IMAGES_BASE_URL || "https://cataas.com/cat/says/"
+const mergeImageName = process.env.MERGE_IAGE_NAME || "cat-card.jpg"
 
 let {
     greeting = 'Hello', who = 'You',
@@ -24,17 +28,17 @@ const run = async (): Promise<void> => {
     try {
         validateInputs()
         // Image one url details
-        const firstRequest: RequestDto = new RequestDto('https://cataas.com/cat/says/',
+        const firstRequest: RequestDto = new RequestDto(baseUrl,
             greeting, width, height, color, size, 'binary')
 
         // Image two url details
-        const secondRequest: RequestDto = new RequestDto('https://cataas.com/cat/says/',
+        const secondRequest: RequestDto = new RequestDto(baseUrl,
             who, width, height, color, size, 'binary')
 
         const imageOne: string = await getImage(firstRequest)
         const imageTwo: string = await getImage(secondRequest)
         const buffer: Buffer = await mergeImage(imageOne, imageTwo, 0, 0, 0, 0);
-        saveImage(`/cat-card.jpg`, buffer)
+        saveImage(mergeImageName, buffer)
     } catch (err) {
         console.log("Exception happened while processing the images")
         console.error(`Reason: ${err}`)
